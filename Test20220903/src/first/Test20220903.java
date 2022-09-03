a941a06ce63b7cf6bed28c640b5031fbedca52a8
klumpra <klumpra@lewisu.edu>
2022-09-03 09:20:15 -0500

    Added a third line.

--- Test20220903/src/first/Test20220903.java
+++ Test20220903/src/first/Test20220903.java
@@ -4,5 +4,6 @@ public class Test20220903 {
 	public static void main(String[] args) {
 		System.out.println("Hey you!");
 		System.out.println("What's up?");
+		System.out.println("How are you?");
 	}
 }

f928cac087405386d0ae85028d0ce3519bea0379
klumpra <klumpra@lewisu.edu>
2022-09-03 08:57:40 -0500

    First commit

---
+++ Test20220903/src/first/Test20220903.java
@@ -0,0 +1,7 @@
+package first;
+
+public class Test20220903 {
+	public static void main(String[] args) {
+		System.out.println("Hey you!");
+	}
+}
---
+++ Test20220903/src/second/Test20220903.java
@@ -0,0 +1,7 @@
+package second;
+
+public class Test20220903 {
+	public static void main(String[] args) {
+		System.out.println("Hey. HOw are you?");
+	}
+}
